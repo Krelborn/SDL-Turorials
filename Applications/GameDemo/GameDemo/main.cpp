8c239d78806d4d42c04f0cd753b81c7f7acac0f6
Matthew Styles <matt@compiledcreations.com>
2018-03-10 13:28:52 +0000

    Tutorial 2: Writing An SDL2 Game Loop
    Added a game loop that reads events, quits, generates random colours and displays them in the window.
    
    Also fixed the plist so that the window support HDPI displays.

--- Applications/GameDemo/GameDemo/main.cpp
+++ Applications/GameDemo/GameDemo/main.cpp
@@ -23,7 +23,53 @@ int main(int argc, const char * argv[])
         return -1;
     }
 
-    cout << "Hooray it works!\n";
+    // Create a window
+    SDL_Window *window = SDL_CreateWindow("Demo Game",
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          640,
+                                          480,
+                                          SDL_WINDOW_OPENGL);
+    if (window == nullptr)
+    {
+        SDL_Log("Could not create a window: %s", SDL_GetError());
+        return -1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr)
+    {
+        SDL_Log("Could not create a renderer: %s", SDL_GetError());
+        return -1;
+    }
+
+    while (true)
+    {
+        // Get the next event
+        SDL_Event event;
+        if (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                break;
+            }
+        }
+
+        // Randomly change the colour
+        Uint8 red = rand() % 255;
+        Uint8 green = rand() % 255;
+        Uint8 blue = rand() % 255;
+
+        // Fill the screen with the colour
+        SDL_SetRenderDrawColor(renderer, red, green, blue, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    // Tidy up
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
